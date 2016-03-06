16b7dbecffaf18fd09a242dd8960428c126f4fe8
Carlos <Carlos@DESKTOP-JHOTQ4U>
2016-03-06 15:21:39 +0100

    agregar una clase llamada hello word y un saludo: hola mundo

---
+++ src/Otros/hello_word.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Otros;
+
+/**
+ *
+ * @author Carlos
+ */
+public class hello_word {
+    public static void main(String[] args) {
+        System.out.println("Hola mundo");
+        System.out.println("como van");
+    }
+}
